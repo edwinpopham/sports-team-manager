661c0bab412cdaf29ebedce11ac37f13954384ad
Edwin Popham <edwinpopham@gmail.com>
2022-11-14 22:19:47 +1000

    ISSUE-1-add-login (#2)
    * add next-auth
    
    * add login
    
    * fix typing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,30 @@
-import type { ReactElement, ReactNode } from 'react'
-import type { NextPage } from 'next'
-import type { AppProps } from 'next/app'
+import type { ReactElement, ReactNode } from "react";
+import type { NextPage } from "next";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import type { Session } from "next-auth";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
+  Component: NextPageWithLayout;
+  pageProps: {
+    session: Session;
+  };
+};
 
-export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+export default function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout ?? ((page) => page)
+  const getLayout = Component.getLayout ?? ((page) => page);
 
-  return getLayout(<Component {...pageProps} />)
+  return getLayout(
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,24 @@
 import type { ReactElement } from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
 import { LoginLayout } from "../src/components/LoginLayout/index";
 import type { NextPageWithLayout } from "./_app";
 
 const Login: NextPageWithLayout = () => {
-  return <p>Login Here</p>;
+  const { data: session } = useSession();
+  if (session !== null && session !== undefined) {
+    return (
+      <>
+        Signed in as {session?.user?.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  );
 };
 
 Login.getLayout = function getLayout(page: ReactElement) {
