9b2b1ab8c3e02607e46c460b319818b1685342f5
edwinpopham <edwinpopham@gmail.com>
2025-08-29 15:40:31 +1000

    Remove swcMinify configuration comment as it is enabled by default in Next.js 16+

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify is enabled by default in Next.js 16+
 }
 
 module.exports = nextConfig
