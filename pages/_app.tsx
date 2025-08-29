161d2c006f1462ab1116a424e443965488097b17
edwinpopham <edwinpopham@gmail.com>
2025-08-29 15:56:05 +1000

    Refactor _app.tsx to simplify component structure and improve readability

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,9 @@
-import type { ReactElement, ReactNode } from "react";
-import type { NextPage } from "next";
-import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
-import type { Session } from "next-auth";
+import type { AppProps } from "next/app";
 
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-  pageProps: {
-    session: Session;
-  };
-};
-
-export default function MyApp({
-  Component,
-  pageProps: { session, ...pageProps },
-}: AppPropsWithLayout) {
-  // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout ?? ((page) => page);
-
-  return getLayout(
-    <SessionProvider session={session}>
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
     </SessionProvider>
   );
